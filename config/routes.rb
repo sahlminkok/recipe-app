198da54e752a213d082c45ac88241eecf722b073
techmoves <muhndlovu28@gmail.com>
2023-09-18 15:52:47 +0200

    Add food,user contoller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'food/index'
+  get 'food/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
