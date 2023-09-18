c7cc6cc885d9d157790f07d351094f60b9dcd9ed
sahlminkok <sahlminkok7@gmail.com>
2023-09-18 20:52:14 +0100

    Add  a route to 'public_recipes#index' for displaying a list of public recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'food/index'
-  get 'food/show'
   root 'food#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :foods, only: [:index, :show]
+  
+  get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
 end
