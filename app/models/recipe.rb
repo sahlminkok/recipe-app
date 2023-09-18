d8a52c2b2fa0e18dc7c0c47b7a81a27883b6ce46
sahlminkok <sahlminkok7@gmail.com>
2023-09-18 17:25:39 +0100

    Create recipe model and add some validations

---
+++ app/models/recipe.rb
@@ -0,0 +1,7 @@
+class Recipe < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :preparation_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :cooking_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
