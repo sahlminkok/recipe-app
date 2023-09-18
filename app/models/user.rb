c97c6909bbc9b277a596bcf33d3d1d9fde050e88
techmoves <muhndlovu28@gmail.com>
2023-09-18 16:06:26 +0200

    Add navbar and install ror linters and bootsrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :recipes, dependent: :destroy
-    has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+  has_many :foods, dependent: :destroy
 
-    validates :name, presence:true
+  validates :name, presence: true
 end
