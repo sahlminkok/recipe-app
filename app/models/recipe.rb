484764c1cac56118c79de5d96abc6618e5babb2e
sahlminkok <sahlminkok7@gmail.com>
2023-09-19 11:54:26 +0100

    Add total_food_items and total_pice methods in recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,12 @@ class Recipe < ApplicationRecord
   validates :name, presence: true
   validates :preparation_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :cooking_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def total_food_items
+    recipe_foods.sum(:quantity)
+  end
+
+  def total_price
+    recipe_foods.joins(:food).sum('recipe_foods.quantity * foods.price')
+  end
 end
