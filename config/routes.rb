9de06a3552dba26642a3cd58d784bc0fe1e3cdfc
sahlminkok <sahlminkok7@gmail.com>
2023-09-19 16:51:29 +0100

    Add recipes routes in routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'foods#index'
 
   resources :foods, only: [:index, :show]
+
+  resources :recipes, only: [:show, :destroy]
+  get '/recipes', to: 'recipes#index', as: 'recipes'
   
   get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
 end
