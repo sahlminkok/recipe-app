618c7c7ef3b1d26503945ec2c533259079e26c30
techmoves <muhndlovu28@gmail.com>
2023-09-19 16:16:15 +0200

    Add create,destroy methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'foods#index'
 
+  root 'foods#index'
   resources :foods, only: [:index, :show]
   
   get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
