4d11a1c30e7e6836217f986e9d77c87d12b1f3dc
sahlminkok <sahlminkok7@gmail.com>
2023-09-20 16:36:35 +0100

    Add toggle_public action to update recipe's public status with response handling.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:toggle_public]
+
   def index
     @recipes = current_user.recipes
   end
@@ -14,6 +16,22 @@ class RecipesController < ApplicationController
       redirect_to recipes_path, notice: 'Recipe deleted successfully!'
     else
       redirect_to recipes_path, alert: 'Error: Recipe could not be deleted'
+    end
+  end
+
+  def toggle_public
+    @recipe = Recipe.find_by(id: params[:id])
+    new_public_state = !@recipe.public
+    if @recipe.update(public: new_public_state)
+      respond_to do |format|
+        format.html { redirect_to recipe_path(@recipe), notice: 'Recipe public status updated successfully!' }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to recipe_path(@recipe), alert: 'Error: Recipe public status could not be updated' }
+        format.js
+      end
     end
   end
 end
