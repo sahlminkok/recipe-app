fdf1ed14c2f98b251a7d404c82d611db86588aae
sahlminkok <sahlminkok7@gmail.com>
2023-09-20 16:40:40 +0100

    Define custom route for toggling public status

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-
   root 'foods#index'
 
   resources :foods, only: [:index, :show, :new, :create, :destroy]
-  resources :shopping_lists, only: [:index]
-  resources :recipes, only: [:show, :destroy]
+
+  # resources :shopping_lists, only: [:index]
+  get '/general_shopping_list', to: 'shopping_lists#index', as: 'general_shopping_list'
+
+  resources :recipes, only: [:show, :destroy] do
+    member do
+      patch 'toggle_public'
+    end  
+  end
   get '/recipes', to: 'recipes#index', as: 'recipes'
 
   get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
