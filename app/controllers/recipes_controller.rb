796f40f432cc7d9e1dc99c5d4d3fe4916caccb24
sahlminkok <sahlminkok7@gmail.com>
2023-09-20 09:10:08 +0100

    Add a destroy actiion in recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,17 @@
 class RecipesController < ApplicationController
   def index
-    @user = current_user
-    @recipes = @user.recipes
+    @recipes = current_user.recipes
   end
 
   def show; end
+
+  def destroy
+    @recipe = current_user.recipes.find_by(id: params[:id])
+
+    if @recipe.destroy
+      redirect_to recipes_path, notice: 'Recipe deleted successfully!'
+    else
+      redirect_to recipes_path, alert: 'Error: Recipe could not be deleted'
+    end
+  end
 end
