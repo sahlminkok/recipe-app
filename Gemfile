71f051e973295becdf2832d7eb434aae9e12b0da
techmoves <muhndlovu28@gmail.com>
2023-09-20 17:26:32 +0200

    Add style,login page and sign up page with devise,

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.2.2'
 
+gem 'devise'
+
 gem 'bootsnap', require: false
 gem 'importmap-rails'
 gem 'jbuilder'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
-
-  def current_user
-    @current_user ||= User.last
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
   end
 end
