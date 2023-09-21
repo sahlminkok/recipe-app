48549741da889ea466d2a8f5919f1b75ddd4ae62
sahlminkok <sahlminkok7@gmail.com>
2023-09-21 17:18:47 +0100

    Add recipe_foods routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   resources :foods, only: [:index, :show, :new, :create, :destroy]
 
-  # resources :shopping_lists, only: [:index]
   get '/general_shopping_list', to: 'shopping_lists#index', as: 'general_shopping_list'
 
   resources :recipes, only: [:show, :destroy] do
     member do
       patch 'toggle_public'
     end  
+    resources :recipe_foods, only: [:new, :create, :destroy]
   end
   get '/recipes', to: 'recipes#index', as: 'recipes'
 
