ac1b9936309f3e2c066bbfc78d824cd2baf0a13e
techmoves <muhndlovu28@gmail.com>
2023-09-22 18:52:48 +0200

    Updated helpers

--- spec/helpers/public_recipes_helper_spec.rb
+++ spec/helpers/public_recipes_helper_spec.rb
@@ -1,15 +1,10 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the PublicRecipesHelper. For example:
-#
-# describe PublicRecipesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe PublicRecipesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#some_helper_method' do
+    it 'returns the correct value' do
+      # Add your test code here
+      expect(helper.some_helper_method).to eq('expected_value')
+    end
+  end
 end
